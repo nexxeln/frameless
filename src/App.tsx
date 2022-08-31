643f194284827231fcc4f2d2988cf4e613691b58
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-31 19:51:13 +0530

    made window frameless

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <main className="flex flex-col h-screen bg-zinc-900 items-center justify-center">
+      <h1 className="text-slate-200 text-4xl font-light">frameless</h1>
+      <p className="text-slate-400 font-light">
+        the browser for all your screenshot needs
+      </p>
+    </main>
   );
 };
 
